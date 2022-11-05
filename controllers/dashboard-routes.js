ac539240efecde14b3325c03d42430bb56d52003
Morgan Kelly <mkelly5380@gmail.com>
2022-11-05 14:55:21 -0500

    on the dashboard added an edit by id route and included custom middleware

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -35,6 +35,40 @@ router.get('/', withAuth, async(req, res) => {
     }catch (err) {
         console.log(err);
         res.status(500).json(err);
-      }
-    
+      } 
+});
+
+//route for a user to edit their posts
+router.get('/edit:id', withAuth, async(req, res) => {
+    try {
+        const dbPostData = await Post.findByPk(req.params.id, {
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'created_at'
+              ],
+              include: [
+                {
+                  model: Comment,
+                  attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                  include: {
+                    model: User,
+                    attributes: ['username']
+                  }
+                },
+                {
+                  model: User,
+                  attributes: ['username']
+                }
+              ]
+        })
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('edit-post', { 
+            posts, 
+            loggedIn: req.session.loggedIn });
+    }catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      } 
 });
