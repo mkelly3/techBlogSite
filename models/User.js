e1f62aca0dbc42793fbafcb7a378136d11ddaa3c
Morgan Kelly <mkelly5380@gmail.com>
2022-11-05 13:30:04 -0500

    used hooks to bcrypt the user password

--- models/User.js
+++ models/User.js
@@ -13,6 +13,57 @@ class User extends Model {
 }
 
 //define tables and columns in the user models
-User.init({
+//User columns include id, username, email and password 
+User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [6],
+        },
+      },
+    },
+    {
+        hooks: {
+            //use the before create to use bcrypt to change the password 
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10)
+                return newUserData;
+            },
+            //calling on bcrypt package to hash the new password 
+            beforeUpdate: async(newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10)
+                return newUserData;
+        }
+    }
+    },
 
-})
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user',
+    }
+  );
+  
+  module.exports = User;
