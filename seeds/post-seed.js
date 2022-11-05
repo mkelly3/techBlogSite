9c1ac9f6683f2d26b572fa1871b427a8c83e8590
Morgan Kelly <mkelly5380@gmail.com>
2022-11-05 13:48:43 -0500

    added seed data for Post

---
+++ seeds/post-seed.js
@@ -0,0 +1,22 @@
+const {Post} = require('../models');
+
+const postData = [
+    {
+        title: "Hello World",
+        content: "First program a devloper should write",
+        user_id: 1
+    },
+    {
+        title: "What is handlebars",
+        content: "It is a javascript library used to create reusable webpage templates",
+        user_id: 2
+    },
+    {
+        title: "How do cookies go stale",
+        content: "A developer can set a time limit in the cookies",
+        user_id: 3
+    }
+];
+
+const seedPost = () => Post.bulkCreate(postData);
+module.exports = seedPost;
