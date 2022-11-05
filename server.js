10bef8a2688b8463182ed0dee9cf1a81f0095906
Morgan Kelly <mkelly5380@gmail.com>
2022-11-05 14:07:58 -0500

    added cookie, session and handlebars to sever.js

--- server.js
+++ server.js
@@ -1,24 +1,51 @@
+// //requiring all paths and packages 
 const express = require('express');
-const exphbs = require('express-handlebars');
-const path = require('path');
 const routes = require('./controllers');
-const hbs = exphbs.create({});
+const sequelize = require('./config/connection');
+const path = require('path');
 
-// Sets up the Express App
+// helper function
+const helpers = require('./utils/helpers');
+
+// handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({helpers});
+
+// session connection to sequelize database
+const session = require('express-session');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set Handlebars as the default template engine.
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 36000
+  },
+  //session will not force the applicition to resave even if nothing has been modified
+  resave: false,
+  //default value for saveUninitialized 
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set Handlebars as the default template engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+// turn on routes
 app.use(routes);
 
-// Starts the server to begin listening
+// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-  });
-  
+  app.listen(PORT, () => console.log('Now listening'));
+});
