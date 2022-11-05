b8db83b2572de03d3fb938c95ea3a847cb07d3c5
Morgan Kelly <mkelly5380@gmail.com>
2022-11-05 14:28:52 -0500

    added login, signup and getting All posts on the homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,6 +6,67 @@ const sequelize = require('../config/connection');
 const {Post, User, Comment} = require('../models');
 const router = require('express').Router();
 
+//getting the homepage 
+router.get('/', async (req,res) => {
+    try {
+        //find all posts 
+        const dbPostData = await Post.findAll({
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'created_at'
+            ],
+            include: [ 
+                {
+                    //all comments for that post and the user 
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                      model: User,
+                      attributes: ['username']
+                    }
+                  },
+                  {
+                    //includ the user who created the post 
+                    model: User,
+                    attributes: ['username']
+                  }
+            ]
+        });
+        const posts = dbPostData.map((post) => 
+            post.get({plain:true})
+        );
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn
+        })
+    }catch (err) {
+        console.log(err);
+        res.status(500).json(err)
+    }
+});
+
+
+
+//route to login 
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+//route to signup
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+
+module.exports = router;
+
 
 
 
