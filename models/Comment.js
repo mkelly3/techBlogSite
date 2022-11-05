8cf09f2db90ee73359e62ac4b94d6461c7dae66e
Morgan Kelly <mkelly5380@gmail.com>
2022-11-05 13:16:36 -0500

    added comment modele including id, commenttext, user id and post id

---
+++ models/Comment.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+class Comment extends Model {}
+
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      comment_text: {
+        type: DataTypes.STRING,
+        validate: {
+            //the comment must be at least 10 characters long
+            len: [10]
+        }
+      },
+      user_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+      },
+      post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'post',
+            key: 'id'
+        }
+    }
+}, {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+});
+module.exports = Comment;
